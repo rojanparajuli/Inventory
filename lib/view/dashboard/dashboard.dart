0c636589140401eb8469a6da799cbfb6aea7a231
rojanparajuli <rojanparajuli13@gmail.com>
2024-05-27 17:23:24 +0545

    dashboard

--- lib/view/dashboard/dashboard.dart
+++ lib/view/dashboard/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/widgets.dart';
@@ -18,8 +19,24 @@ class Dashboard extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           mainAxisSize: MainAxisSize.min,
           children: [
+            Container(
+              width: 250,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    suffix: Icon(Icons.search),
+                              labelText: 'search inventory',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(50))
+                              ),
+                            ),
+                ),
+              ),
+            ),
             SizedBox(
               height: 300,
               child: Padding(
@@ -36,7 +53,7 @@ class Dashboard extends StatelessWidget {
                         ),
                         PieChartSectionData(
                           color: Colors.green,
-                          value: itemController.totalWorth,
+                          value: itemController.totalWorth.toDouble(),
                           title: 'Total Worth',
                           radius: 50,
                         ),
@@ -52,86 +69,95 @@ class Dashboard extends StatelessWidget {
                 }),
               ),
             ),
-            SizedBox(
-              height: 400,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Obx(() {
-                  return BarChart(
-                    BarChartData(
-                      alignment: BarChartAlignment.spaceAround,
-                      maxY: 400,
-                      barTouchData: BarTouchData(enabled: false),
-                      titlesData: FlTitlesData(
-                        show: true,
-                        bottomTitles: AxisTitles(
-                          sideTitles: SideTitles(
-                            showTitles: true,
-                            getTitlesWidget: (double value, TitleMeta meta) {
-                              const style = TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 14,
-                              );
-                              Widget text;
-                              switch (value.toInt()) {
-                                case 0:
-                                  text = const Text('Total Quantity', style: style);
-                                  break;
-                                case 1:
-                                  text = const Text('Total Worth', style: style);
-                                  break;
-                                case 2:
-                                  text = const Text('Total Sell', style: style);
-                                  break;
-                                default:
-                                  text = const Text('');
-                                  break;
-                              }
-                              return SideTitleWidget(
-                                axisSide: meta.axisSide,
-                                child: text,
-                              );
-                            },
-                            reservedSize: 35,
+            SizedBox(height: 130,),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                color: Colors.red,
+                height: 200,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Obx(() {
+                    print(itemController.totalWorth);
+                    print(itemController.totalQuantity);
+                    print(itemController.totalSell);
+                    return BarChart(
+                      BarChartData(
+                        alignment: BarChartAlignment.spaceAround,
+                        maxY: 100,
+
+                        barTouchData: BarTouchData(enabled: false,),
+                        titlesData: FlTitlesData(
+                          show: true,
+                          bottomTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                              showTitles: true,
+                              getTitlesWidget: (double value, TitleMeta meta) {
+                                const style = TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14,
+                                );
+                                Widget text;
+                                switch (value.toInt()) {
+                                  case 0:
+                                    text = const Text('Total Quantity', style: style);
+                                    break;
+                                  case 1:
+                                    text = const Text('Total Worth', style: style);
+                                    break;
+                                  case 2:
+                                    text = const Text('Total Sell', style: style);
+                                    break;
+                                  default:
+                                    text = const Text('');
+                                    break;
+                                }
+                                return SideTitleWidget(
+                                  axisSide: meta.axisSide,
+                                  child: text,
+                                );
+                              },
+                              reservedSize: 35,
+                            ),
+                          ),
+                          leftTitles: const AxisTitles(
+                            sideTitles: SideTitles(showTitles: false),
                           ),
                         ),
-                        leftTitles: const AxisTitles(
-                          sideTitles: SideTitles(showTitles: false),
-                        ),
+                        barGroups: [
+                          BarChartGroupData(
+                            x: 0,
+                            barRods: [
+                              BarChartRodData(
+                                toY: itemController.totalQuantity.toDouble(),
+                                color: Colors.blue,
+                              ),
+                            ],
+                          ),
+                          BarChartGroupData(
+                            x: 1,
+                            barRods: [
+                              BarChartRodData(
+                                toY: itemController.totalWorth,
+                                color: Colors.green,
+                              ),
+                            ],
+                          ),
+                          BarChartGroupData(
+                            x: 2,
+                            barRods: [
+                              BarChartRodData(
+                                toY: itemController.totalSell.toDouble(),
+                                color: Colors.red,
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                      barGroups: [
-                        BarChartGroupData(
-                          x: 0,
-                          barRods: [
-                            BarChartRodData(
-                              toY: itemController.totalQuantity.toDouble(),
-                              color: Colors.blue,
-                            ),
-                          ],
-                        ),
-                        BarChartGroupData(
-                          x: 1,
-                          barRods: [
-                            BarChartRodData(
-                              toY: itemController.totalWorth,
-                              color: Colors.green,
-                            ),
-                          ],
-                        ),
-                        BarChartGroupData(
-                          x: 2,
-                          barRods: [
-                            BarChartRodData(
-                              toY: itemController.totalSell.toDouble(),
-                              color: Colors.red,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  );
-                }),
+                    );
+                  }),
+                ),
               ),
             ),
           ],
